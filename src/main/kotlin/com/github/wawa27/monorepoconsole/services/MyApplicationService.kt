69bf764864c2468f1726ad06760acdc7b955beb2
GitHub Action <action@github.com>
2022-06-30 12:30:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/wawa27/monorepoconsole/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wawa27/monorepoconsole/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wawa27.monorepoconsole.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wawa27.monorepoconsole.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wawa27/monorepoconsole/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wawa27/monorepoconsole/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wawa27.monorepoconsole.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wawa27.monorepoconsole.MyBundle
 
 class MyProjectService(project: Project) {
 
